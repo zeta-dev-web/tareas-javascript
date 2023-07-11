e63198b35057762bad022b76cc2c9ce0320267d6
Leonardo Zamorano <nvreparaciones@gmail.com>
2023-07-10 23:22:40 -0300

    correccion de tarea 10/07

--- js/tarea-10-07.js
+++ js/tarea-10-07.js
@@ -1,15 +1,5 @@
 // 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
-// Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
-// let frase = prompt("Ingrese una frase")
-// let vocalesValidas = ["a","e","i","o","u","á","é","í","ó","ú"]
-// let vocalesEnFrase = ""
 
-// for (let index = 0; index < frase.length; index++) {
-//      let letra = frase[index].toLowerCase();
-//    if (letra.includes(vocalesValidas)) {
-//    vocalesEnFrase.push(letra)
-//    } 
-// }
 let frase = prompt("Ingrese una frase:");
 let vocales = ["a","e","i","o","u","á","é","í","ó","ú"];
 let vocalesEncontradas = [];
